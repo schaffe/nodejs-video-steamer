30e8e07f78ad02129bb2c7181b1600221c81ac68
dzidzoiev <artur.dzidzoiev@gmail.com>
2018-01-31 15:34:57 +0100

    caching files

--- src/index.js
+++ src/index.js
@@ -1,57 +1,26 @@
 "use strict";
 const cheerio = require("cheerio");
 const express = require('express');
+const movie = require("./movie");
 
 var request = require('request');
 request = request.defaults({jar: true});
 
 const app = express();
 
-let movies = {};
-
 app.get('/', (req, res) => {
-    getDriveUrl("0ByhbuWRNNPUyaGhhb2RGWHA3SEk", res)
+    movie.getDriveUrl("0ByhbuWRNNPUyaGhhb2RGWHA3SEk", res)
+});
+
+app.get('/:season/', (req, res) => {
+    movie.crawl(req.param("season")).then((result) => res.send(result))
 });
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
 
-let getMovieUrl = function (url) {
-    request(url, (error, response, html) => {
-        const $ = cheerio.load(html);
-        const url = $('iframe').filter(function (i, el) {
-            // this === el
-            return $(this).attr('src').indexOf('drive.google.com') > -1;
-        }).attr('src');
-        console.log(url);
-    });
-};
-
-let getDriveUrl = function (id, res) {
-    const url = "https://drive.google.com/uc?export=download&id=" + id;
-    let jar = request.jar();
-    request({url, jar}, (error, response, html) => {
-        const $ = cheerio.load(html);
-        const url1 = "https://drive.google.com" + $('#uc-download-link').attr('href');
-        var cookie_string = jar.getCookieString(url); // "key1=value1; key2=value2; ..."
-        var cookies = jar.getCookies(url);
-        console.log(url1);
-        request({url: url1, jar}).pipe(res);
-    });
-};
-
-const crawl = (sezon = 22) => {
-    request("http://simpsonsua.com.ua/sezon-" + sezon +"/", (error, response, html) => {
-        const $ = cheerio.load(html);
-        $('.movie_item figure a').each(function(i, elem) {
-            let url = $(this).attr('href');
-            console.log(url);
-            console.log("--> " + getMovieUrl(url))
-        });
-    });
-};
-
 //
-// crawl();
+movie.crawl().then((movies) => console.log(movies));
+// crawl().then(() => console.log(movies));
 
 // getDriveUrl("0ByhbuWRNNPUyaGhhb2RGWHA3SEk");
 // getDriveUrl("https://drive.google.com/file/d/0ByhbuWRNNPUyaGhhb2RGWHA3SEk");
---
+++ src/movie.js
@@ -0,0 +1,61 @@
+"use strict";
+const cheerio = require("cheerio");
+const request = require('request').defaults({jar: true});
+
+const self = (() => {
+    const movies = {};
+    return {
+        getMovieUrl: function (url) {
+            return new Promise((resolve, reject) => {
+                request(url, (error, response, html) => {
+                    const $ = cheerio.load(html);
+                    const url = $('iframe').filter(function (i, el) {
+                        // this === el
+                        return $(this).attr('src').indexOf('drive.google.com') > -1;
+                    }).attr('src');
+                    if (!url)
+                        reject("Movie URL is null");
+
+                    return resolve(url);
+                })
+            })
+        },
+
+        getDriveUrl: function (id, res) {
+            const url = "https://drive.google.com/uc?export=download&id=" + id;
+            let jar = request.jar();
+            request({url, jar}, (error, response, html) => {
+                const $ = cheerio.load(html);
+                const url1 = "https://drive.google.com" + $('#uc-download-link').attr('href');
+                console.log(url1);
+                request({url: url1, jar}).pipe(res);
+            });
+        },
+
+        crawl: (sezon = 22) => {
+            if (movies[sezon] === undefined) {
+                let seasonMovie = {};
+                movies[sezon] = seasonMovie;
+                return new Promise((resolve, reject) => {
+                    request("http://simpsonsua.com.ua/sezon-" + sezon + "/", (error, response, html) => {
+                        const $ = cheerio.load(html);
+                        const promises = [];
+                        $('.movie_item figure a').map(function (i, elem) {
+                            let url = $(this).attr('href');
+                            console.log(url);
+                            promises.push(self.getMovieUrl(url)
+                                .then((url) => seasonMovie[i] = url)
+                                .then(console.log)
+                                .catch(err => console.error(err)));
+                        });
+                        return Promise.all(promises).then(() => resolve(movies[sezon])).catch(reject);
+                    })
+                }).catch(console.error);
+            } else {
+                return Promise.resolve(movies[sezon]);
+            }
+        }
+    }
+})();
+
+module.exports = self;
